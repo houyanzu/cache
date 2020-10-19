5a0836147a1abacae97da7d34aad3dac40abc0dd
houjia <you@example.com>
2020-10-19 17:55:20 +0800

    .

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/astaxie/beego/cache"
+	"github.com/houyanzu/cache"
 	"github.com/gomodule/redigo/redis"
 )
 
--- ssdb/ssdb_test.go
+++ ssdb/ssdb_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/astaxie/beego/cache"
+	"github.com/houyanzu/cache"
 )
 
 func TestSsdbcacheCache(t *testing.T) {
